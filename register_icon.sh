98cd2102224b82210d32703a4ddc0e75f85217a1
Rodrigo Daunoravicius <rodelrod@putoperdiz>
2014-02-19 23:41:04 +0100

    Added slightly bigger icon

--- register_icon.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-xdg-icon-resource install --size 16 vpn-applet.png vpn-applet
---
+++ register_icons.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+xdg-icon-resource install --size 16 icons/vpn-applet.png vpn-applet
+xdg-icon-resource install --size 32 icons/vpn-applet-32.png vpn-applet
